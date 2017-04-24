d37300eac1fd86f9b54cbc52f338a5fa4e0f2448
Håvar Aambø Fosstveit <havar@fosstveit.net>
2017-04-24 11:23:47 +0200

    Working mediahandler

--- server.js
+++ server.js
@@ -1,21 +1,18 @@
-// Simple registrar - redirector
-//
-
 var SIP = require('sip.js');
 var kurento = require('kurento-client');
 var util = require('util');
 var fs = require('fs');
 var nodemediahandler = require('./mediahandler.js')
 
-let ua = new SIP.UA({
+var ua = new SIP.UA({
     traceSip: true,
     register: true,
     uri: "websip@webrtcsip.uninett.no",
     password: "websip",
-    rel100: SIP.C.supported.SUPPORTED,
-    wsServers: "wss://webrtcsip.uninett.no:443",
+    rel100: SIP.C.supported.UNSUPPORTED,
+    wsServers: "wss://webrtcsip.uninett.no",
     turnServers: {
-        urls: ["stun:stun.l.google.com:19302", "turn:videoturn@158.38.2.18:443?transport=tcp"],
+        urls: ["turn:videoturn@158.38.2.18:443?transport=tcp"],
         username: "videoturn",
         password: "videoturn"
     },
@@ -23,4 +20,7 @@ let ua = new SIP.UA({
     mediaHandlerFactory: nodemediahandler.nodeMediaHandlerFactory
 });
 
-ua.start();
+ua.on('invite', function(session) {
+    console.log("INVITE arrived!");
+    session.accept();
+});
